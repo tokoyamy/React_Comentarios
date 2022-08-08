da0aa60ea82085cbcf45121a22e086570569bf02
Tokoyamy <math.felipemachado@gmail.com>
2022-08-07 22:36:38 -0300

    Add Comment

---
+++ src/components/Comentario.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Comentario.css";
+//JSX
+const Comentario = () => (
+  <div className="Comentario">
+    <div>Matheus:</div>
+    <div> Olá, tudo bem?</div>
+  </div>
+);
+export default Comentario;
